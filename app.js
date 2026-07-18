22ee947425c78a14ef130e604ecd462912a93d46
Yidnekachew-SK <yidnekachewsk@gmail.com>
2026-07-18 15:12:39 +0300

    Install necessary packages

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require("node:path");
+
+const app = express();
+const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log('Error', error);
+    }
+
+    console.log('server running');
+})
